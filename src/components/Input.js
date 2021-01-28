e2c03280e3369f3a1f4a55c4b4e8c280a7718572
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-01-27 22:35:19 -0800

    Finished input

--- src/components/Input.js
+++ src/components/Input.js
@@ -38,31 +38,36 @@ import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
+  const [inputValue, setInputValue] = useState();
   
   const changeInput = evt => {
     
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
+    // Set the value of the input -found inside the event object- into state.
+    setInputValue(value)
     /* STEP 4 */
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue(" ")
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color:'royalblue', /* STEP 2 */
+    textTransform:'uppercase',
+    color: inputValue > 11 ? 'crimson' : 'royalblue', /* STEP 2 */
+    // Make the color of the text be crimson if the length of 'inputValue' goes over ten.
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+  <div id='output' style={style} >{inputValue}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
